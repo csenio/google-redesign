44431fb618e1575fe65a29917da955cd30dbb060
Jesco Wuester <jescowuester@gmail.com>
2019-01-16 22:47:24 +0100

    Updates

---
+++ precache-manifest.c93ec04f20f7ddbb6df5330a8d79ab63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84a37bdc36524f1ad787",
+    "url": "%20//jescowuester.github.io/google-redesign/static/css/main.92ec80d1.chunk.css"
+  },
+  {
+    "revision": "84a37bdc36524f1ad787",
+    "url": "%20//jescowuester.github.io/google-redesign/static/js/main.84a37bdc.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "%20//jescowuester.github.io/google-redesign/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "1807f3f37dff19489cea",
+    "url": "%20//jescowuester.github.io/google-redesign/static/js/runtime~main.1807f3f3.js"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "%20//jescowuester.github.io/google-redesign/static/media/Product Sans Regular.eae9c18c.ttf"
+  },
+  {
+    "revision": "9a29b00c2e02b7a43fd1c31de8f5d841",
+    "url": "%20//jescowuester.github.io/google-redesign/index.html"
+  }
+];
